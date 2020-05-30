0257e4c1b22adf6438a37c27b9971cb06caf0d45
Priscila Gutierres <priscila.gutierres@gmail.com>
2020-05-30 13:06:46 -0300

    first working version main.py is now ps1match.py

--- ps1match.py
+++ ps1match.py
@@ -13,11 +13,14 @@ constraints = {'nDetections.gt':1}
 fwrite = sys.argv[2]
 f = open(fwrite,"w+")
 # strip blanks and weed out blank and commented-out values
-columns = """objID,raMean,decMean,nDetections,ng,nr,ni,nz,ny,
-    gMeanPSFMag,rMeanPSFMag,iMeanPSFMag,zMeanPSFMag,yMeanPSFMag""".split(',')
+columns = "objID,raMean,decMean,nDetections,ng,nr,ni,nz,ny," + \
+    "gMeanPSFMag,rMeanPSFMag,iMeanPSFMag,zMeanPSFMag,yMeanPSFMag"
+    
+print("Starting....")
+f.write(columns + '\n')
+columns = columns.split(',')
 columns = [x.strip() for x in columns]
 columns = [x for x in columns if x and not x.startswith('#')]                                                 
-
 for index, row in data.iterrows():
     results = None
     while results is None:
@@ -25,12 +28,11 @@ for index, row in data.iterrows():
             ra = row['ra']
             dec = row['dec']
             results = lib.ps1cone(ra,dec,radius,release='dr2',columns=columns,
-                                  verbose=True,**constraints)
+                                  verbose=False,**constraints)
             lines = results.split('\n')
             print(len(lines),"rows in results -- first 5 rows:")
-            print('\n'.join(lines[:6]))  
-            for line in lines:
-                f.write(line)
+            print('\n'.join(lines[1:6]))  
+            f.write('\n'.join(lines[1:]))
         except ConnectionError: 
             print("Connection Error" )
             print("Retrying...")

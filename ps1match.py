67154e4586d92054b25b805e39b05d4983fcd46f
Priscila Gutierres <priscila.gutierres@gmail.com>
2020-06-19 16:24:08 -0300

    Added support to restart Some bugs were fixed

--- ps1match.py
+++ ps1match.py
@@ -1,5 +1,7 @@
 import lib.functions as lib
 from astropy.table import Table
+from time import sleep
+import numpy as np
 from requests.exceptions import ConnectionError, Timeout, ConnectTimeout
 import configparser
 import os, sys
@@ -36,19 +38,40 @@ print("Starting....")
 f.write(columns.replace("\n"," ") + "\n")
 columns = columns.split(',\n')
 columns = [x.strip() for x in columns]
-columns = [x for x in columns if x and not x.startswith('#')]                                                 
+columns = [x for x in columns if x and not x.startswith('#')]
+
+length = len(list(data.iterrows()))
+array = np.zeros(2*length)
+i = 0 
+j = 0
+
 for index, row in data.iterrows():
+    array[i]= row['ra'] 
+    array[i+1] = row['dec']
+    i += 2
+
+array = array.reshape(length,2)
+
+if (os.path.exists(sys.argv[2]) == True):
+    fopen = open(sys.argv[2],"r")
+    j = int(fopen.readline())
+    fopen.close()
+print(str(j))                        
+for line in range(j,length):
     results = None
     while results is None:
         try: 
-            ra = row['ra']
-            dec = row['dec']
+            ra = array[line][0]
+            dec = array[line][1]
             results = lib.ps1cone(ra,dec,radius,release=release,table=table,
                                   columns=columns, verbose=False,**constraints)
+            fopen = open(sys.argv[2],"w+")
+            fopen.write(str(j))
+            fopen.close()
             lines = results.split('\n')
-            print(len(lines)-1,"rows in results -- first 5 rows:")
-            #print('\n'.join(lines[1:6]))  
+            print(str(j/length*100)," completed")
             f.write('\n'.join(lines[1:]))
+            j += 1
         except ConnectionError: 
             print("Connection Error" )
             print("Retrying...")
@@ -64,6 +87,6 @@ for index, row in data.iterrows():
             sys.exit()
         except KeyboardInterrupt:
             sys.exit()
-        except:
-            print("Unknown error..")
-            print("Retrying...")
+#        except:
+#            print("Unknown error..")
+#            print("Retrying...")
